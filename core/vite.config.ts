a039e2178f4a8ba1ae538eea0633a1c6fbf11bdc
jianghong <jianghong2020@qq.com>
2024-08-16 15:05:50 +0800

    部署demo

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import { resolve } from 'path';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-export default defineConfig({
-  plugins: [
-    solid(),
-    cssInjectedByJsPlugin()
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/solana-modal.tsx'),
-      name: 'SolanaModal',
-      formats: ['cjs', 'es', 'umd'],
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    plugins: [
+      solid(),
+      cssInjectedByJsPlugin()
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/solana-modal.tsx'),
+        name: 'SolanaModal',
+        formats: ['cjs', 'es', 'umd'],
+      },
     },
-  },
-  css: {
-    postcss: './postcss.config.js'
-  },
-  optimizeDeps: {
-    force: true,
-  },
+    css: {
+      postcss: './postcss.config.js'
+    },
+    optimizeDeps: {
+      force: true,
+    },
+  }
+  if (mode == 'demo') {
+    delete config.build?.lib
+  }
+  return config
 })
